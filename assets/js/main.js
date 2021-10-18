d8d455232a6cf5a15996a4f28240b1b836d42155
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2021-10-18 21:43:03 +0000

    Add func for reset cards & func to identify the img src

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,44 @@
 // Global scope Variables
 let cards = Array.from(document.getElementsByClassName('cards'));
 let flipCounter = document.getElementById('flips-counter');
-let flips = 0;
+let flips;
 let timer = document.getElementById('game-timer');
 let timeLeft = 90;
 let pairedCards = [];
+let verifyCard = null;
 let canClick = true;
 let cardsArray = cards;
 
+
 // Start game function to initialise the components for the game
 function startGame() { 
-    flips;
+    flips = 0;
     pairedCards;
-    console.log(pairedCards)
+    console.log(pairedCards);
 
-    cards.forEach(card => {
-        card.addEventListener('click', () => {
-            rotateCard(card)
-        })
-    })
+    cards.forEach((card) => {
+            card.addEventListener('click', () => {
+                rotateCard(card);
+                getCard(card);
+            });
+        }) 
 
-    setTimeout(() =>{
-        shuffleCards(cardsArray);
-        canClick = false;
-        timeRemaining();
-    },500)
+    // Setimeout created to delay for half a second, allow for shuffle of cards and start the time remaining timer and can click boolean.    
+    setTimeout(() => {
+            shuffleCards(cardsArray);
+            canClick = false;
+            timeRemaining();
+        },500)
 }
-
 startGame();
 
 function rotateCard(card) {
-    flips++;
-    flipCounter.innerText = flips;
-    card.classList.add('visible');
+    if(canFlipCard(card)){
+        flips++;
+        flipCounter.innerText = flips;
+        card.classList.add('visible');
+    }
+    
 }
 
 
@@ -59,6 +65,24 @@ function timeRemaining() {
         },1000);
 }
 
+// Reset cards to ensure all cards are at starting position, and to reset after a game played.
+function resetCards() {
+    cardsArray.forEach(item =>{
+        item.classList.remove('visible');
+    })
+}
+
+function canFlipCard(card){
+    return true;
+}
+
+// Function to  return the card clicked src to be checked against another card
+function getCard(card){
+    return card.getElementsByClassName('deck-card')[0].src;
+}   
 
 
+function checkCardsMatch(card1, card2){
+
+}
 
