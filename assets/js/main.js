3cb2d412b144935c8d9292586c747546bb2f8c3c
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2021-10-20 21:31:58 +0000

    Update comments on functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,8 @@ let tryAgainText = document.getElementsByClassName('try-again-container')[0];
 let victoryText = document.getElementsByClassName('victory-container')[0];
 let overallScore = document.getElementsByClassName('score-overall')[0]
 let flipCounter = document.getElementById('flips-counter');
-let flips;
 let timer = document.getElementById('game-timer');
+let flips;
 let timeLeft;
 let pairedCards = [];
 let verifyCard = null;
@@ -18,30 +18,32 @@ let cardsArray = cards;
 // Start game function to initialise the components for the game
 function startGame() { 
     flips = 0;
-    timeLeft = 120;
+    timeLeft = 90;
     pairedCards;
     resetCards();
     overlay.classList.add('hide');
     tryAgainText.classList.remove('show');
     victoryText.classList.remove('show')
-
+    // For each card found in cards array add an e listener when clicked call rotatecard func passing in the e card.
     cards.forEach((card) => {
             card.addEventListener('click', () => {
                 rotateCard(card);
-                getCard(card);
             });
-        }) 
+        })
 
-    // Setimeout created to delay for half a second, allow for shuffle of cards and start the time remaining timer and can click boolean.    
+    // Setimeout created to delay for a second, allow for shuffle of cards and start the time remaining timer and can click boolean.    
     setTimeout(() => {
             shuffleCards(cardsArray);
             canClick = false;
             stopCounter = countdownTimer();
-
         },1000)
 }
-startGame();
 
+/*
+function to rotate a card when clicked wrapped in condition to handle user interaction, 
+increment flips per click, update flipcounter with each increment.
+if true call func, else the clicked card is now the verify card var.
+*/
 function rotateCard(card) {
     if(canClickCard(card)){
         flips++;
@@ -54,10 +56,10 @@ function rotateCard(card) {
             //assign current card (card) to the verify card
             verifyCard = card;
     }
-
 }
 
-// Fisher-Yates Shuffle Algorithm
+
+//Fisher-Yates Shuffle Algorithm to shuffle the cards by the order positioned in the css grid.
 function shuffleCards(cardsArray) { 
     for (let i = cardsArray.length - 1; i > 0; i--) {
         let randIndex = Math.floor(Math.random() * (i + 1));
@@ -66,7 +68,7 @@ function shuffleCards(cardsArray) {
     }
 }
 
-// Timer count down function
+// Timer count down function, decrement the timer every 1 second using setInterval method, when timer == 0 we call try again function.
 function countdownTimer() {
     return setInterval(() => {
             timeLeft--;
@@ -77,30 +79,45 @@ function countdownTimer() {
         },1000);
 }
 
-// Reset cards to ensure all cards are at starting position, and to reset after a game played.
+// Reset cards to ensure all cards are at starting position, and to reset after a game played, for each card remove the visible class if applied.
 function resetCards() {
     cardsArray.forEach(item =>{
         item.classList.remove('visible');
     })
 }
 
-// Function to  return the card clicked src to be checked against another card
+/*
+Function to return the current card clicked src, 
+and to  be checked against another card src.
+takes param of card which is passed as the currently clicked card.
+*/
 function getCard(card){
     return card.getElementsByClassName('deck-card')[0].src;
 }   
 
-
+/*
+function to check if cards match,
+calling the getCard function and passing the current card and checking vs the stored card,
+if true cardsMatch is called which pushes clicked card and stored card to array.
+else, cards dont match is called with the current clicked card and stored card and removes the visible class.
+then clearing the verifyCard variable by initializing it back to null.
+*/
 function checkCardsMatch(card) {
     if(getCard(card) === getCard(verifyCard))
         cardsMatch(card, verifyCard);
     else
         cardsDontMatch(card, verifyCard);
 
-    verifyCard = null;    
-    console.log(cardsArray)
-    console.log(pairedCards)
+    verifyCard = null;
 }
 
+
+/*
+Function that takes 2 parameters card1 and card 2,
+in this case being the clicked card and the stored card.
+can click set to true, and used in another return function to stop interaction,
+in a timeout method we wait 1 second, remove the visible class and reset canclick to false.
+*/
 function cardsDontMatch(card1, card2) {
     canClick = true;
     setTimeout(() => {
@@ -110,15 +127,23 @@ function cardsDontMatch(card1, card2) {
     },1000)
 }
 
+/*
+Function to handle pushing specifically passed cards to the pairdCards array
+then check if the pairedcards array length is the same as the cardsArray length
+if condition is met call gameOver function.
+*/
 function cardsMatch(card1, card2) {
     pairedCards.push(card1);
     pairedCards.push(card2);
 
     if(pairedCards.length === cardsArray.length)
-    //victory
         gameOver();
 }
 
+/*Gamover function to stop the current interval from the timer
+and and remove css classes for interactions and update the overallscore innertext
+with the flips counter number.
+*/
 function gameOver() {
     //reset timer
     clearInterval(stopCounter);
@@ -127,12 +152,22 @@ function gameOver() {
     overallScore.innerText = flips;
 }
 
+/* 
+Try Again function which clears the intervals from the timer
+remove css to show engame overlay and show the try again text.
+*/
 function tryAgain(){
-    //show try again div
     clearInterval(stopCounter);
     overlay.classList.remove("hide");
     tryAgainText.classList.add('show');
 }
+
+/*
+Can click function to return true if the conditions are met,
+checking if card clicked as parameter is not included in the pairdcards
+and the card is not == to the verifyCard, once all 3 conditions are me we 
+get a true boolean and we can call this to disable user interaction.
+*/
 
 function canClickCard(card) {
     return !canClick && !pairedCards.includes(card) && card !== verifyCard;
